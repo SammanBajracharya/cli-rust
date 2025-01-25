41cb14fc04ae68a0ebd64d9257c97813f81f8af2
Samman Bajracharya <sammanbajracharya@gmail.com>
2025-01-25 22:13:16 +0545

    rls does something

--- rls/src/main.rs
+++ rls/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut paths: Vec<String> = Vec::new();
+    for entry in fs::read_dir(".").unwrap() {
+        let dir = entry.unwrap();
+        let path = dir.path();
+
+        let path_str = path.to_string_lossy();
+        let components: Vec<&str> = path_str.split('/').collect();
+        if components.len() > 1 {
+            paths.push(components[1].to_string());
+        }
+    }
+
+    paths.sort();
+
+    for path in paths {
+        print!("{}  ", path);
+    }
+    println!();
 }
