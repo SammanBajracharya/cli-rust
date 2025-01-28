f8b4918a0d98aca47fb250d6120c55e51e3e92cf
Samman Bajracharya <sammanbajracharya@gmail.com>
2025-01-28 16:56:18 +0545

    file based color

--- rls/src/main.rs
+++ rls/src/main.rs
@@ -1,22 +1,34 @@
 use std::fs;
+use colored::*;
 
 fn main() {
-    let mut paths: Vec<String> = Vec::new();
+    // (file type, file name)
+    let mut paths: Vec<(String, fs::FileType)> = Vec::new();
     for entry in fs::read_dir(".").unwrap() {
         let dir = entry.unwrap();
         let path = dir.path();
 
         let path_str = path.to_string_lossy();
         let components: Vec<&str> = path_str.split('/').collect();
-        if components.len() > 1 {
-            paths.push(components[1].to_string());
+        if let Ok(file_type) = dir.file_type() {
+            if components.len() > 1 {
+                paths.push((components[1].to_string(), file_type));
+            }
         }
     }
 
-    paths.sort();
+    // How to sort only 1st part
+    paths.sort_by(|a, b| a.0.cmp(&b.0));
 
     for path in paths {
-        print!("{}  ", path);
+        let file_name: String = if path.1.is_file() {
+            format!("{}  ", path.0.white())
+        } else if path.1.is_dir() {
+            format!("{}  ", path.0.blue())
+        } else {
+            format!("{}  ", path.0.red())
+        };
+        print!("{} ", file_name);
     }
     println!();
 }
